f101fd32ae30b6f5ebcbdbe5fefac66e92a219c7
otaki0413 <takida0413@gmail.com>
2023-07-16 16:40:33 +0900

    📝 各種設定ファイルにコメント追記

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const config = {
-  reactStrictMode: true,
-  poweredByHeader: false,
-  experimental: { typedRoutes: true },
+  reactStrictMode: true, // Reactの厳格モードを有効
+  poweredByHeader: false, // X-Powered-Byヘッダーをレスポンスから完全に削除、サーバーサイドの情報を隠す
+  experimental: { typedRoutes: true }, // 静的型付けリンクのサポート有効
 };
 
 export default config;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const config = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"], // **は0個以上のディレクトリ、*は0個以上のファイルを表す
   theme: {},
   plugins: [],
 };
